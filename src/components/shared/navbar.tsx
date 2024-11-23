10982c31779913b127d5d434fec15888faaa277d
gourab8389 <deyg6988@gmail.com>
2024-11-23 22:32:44 +0530

    Add SearchBar component and integrate it into the navbar

--- src/components/shared/navbar.tsx
+++ src/components/shared/navbar.tsx
@@ -1,18 +1,19 @@
 import { LocationDialog } from "../landing/location-dialog";
+import SearchBar from "../landing/search-bar";
 
 const Navbar = () => {
   return (
-    <section className='w-full max-w-screen-2xl px-5 md:px-14 flex gap-5 items-center justify-center fixed top-0 left-0 right-0 mx-auto shadow-sm h-[5rem]'>
-        <div className="w-[30%] flex items-center justify-between">
+    <section className='w-full max-w-screen-2xl px-5 md:px-14 flex md:gap-10 justify-between items-center md:justify-center fixed top-0 left-0 right-0 mx-auto shadow-sm h-[5rem]'>
+        <div className="w-[25%] flex items-center justify-between">
             <div className="flex items-center justify-center">
             <h1 className='text-sky-950 font-extrabold text-4xl'>Logo</h1>
             </div>
             <LocationDialog/>
         </div>
-        <div className="w-[20%] hidden md:flex gap-5">
-
+        <div className="w-[30%] hidden md:flex gap-5">
+            <SearchBar/>
         </div>
-        <div className="w-[50%]"></div>
+        <div className="w-[45%]"></div>
     </section>
   )
 }
