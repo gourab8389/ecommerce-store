04457c6cda83a91aaf56369097ee139470de2af8
gourab8389 <deyg6988@gmail.com>
2024-11-23 21:47:24 +0530

    Add LocationDialog component and dialog UI elements to navbar

--- src/components/shared/navbar.tsx
+++ src/components/shared/navbar.tsx
@@ -1,16 +1,18 @@
+import { LocationDialog } from "../landing/location-dialog";
+
 const Navbar = () => {
   return (
     <section className='w-full max-w-screen-2xl px-5 md:px-14 flex gap-5 items-center justify-center fixed top-0 left-0 right-0 mx-auto shadow-sm h-[5rem]'>
-        <div className="w-[40%] flex items-center justify-center">
-            <div className="">
+        <div className="w-[30%] flex items-center justify-between">
+            <div className="flex items-center justify-center">
             <h1 className='text-sky-950 font-extrabold text-4xl'>Logo</h1>
             </div>
-            <div className=""></div>
+            <LocationDialog/>
         </div>
         <div className="w-[20%] hidden md:flex gap-5">
 
         </div>
-        <div className="w-[40%]"></div>
+        <div className="w-[50%]"></div>
     </section>
   )
 }
