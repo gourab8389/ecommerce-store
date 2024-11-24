2e72a6e778170c4560368909588987b8c99aa237
gourab8389 <deyg6988@gmail.com>
2024-11-24 17:03:55 +0530

    Refactor Navbar layout and add Sheet component for improved UI; implement Sheet functionality with variants

--- src/components/shared/navbar.tsx
+++ src/components/shared/navbar.tsx
@@ -5,7 +5,8 @@ import SearchBar from "../landing/search-bar";
 
 const Navbar = () => {
   return (
-    <section className='w-full max-w-screen-2xl px-5 md:px-14 flex md:gap-10 justify-between items-center md:justify-center fixed top-0 left-0 right-0 mx-auto shadow-sm h-[5rem]'>
+    <div className="w-full h-[5rem] fixed top-0 left-0 right-0 mx-auto shadow-md flex items-center justify-center">
+          <section className='w-full max-w-screen-2xl px-5 md:px-14 flex md:gap-10 justify-between items-center md:justify-center h-full'>
         <div className="w-[25%] flex items-center justify-between">
             <div className="flex items-center justify-center">
             <h1 className='text-sky-950 font-extrabold text-4xl'>Logo</h1>
@@ -20,6 +21,7 @@ const Navbar = () => {
             <UserDropdownMenu/>
         </div>
     </section>
+    </div>
   )
 }
 
